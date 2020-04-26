2dcd8773554728dcd6c087c313d05d9ae26e19c1
red <tancros.janos@gmail.com>
2020-04-26 23:31:51 +0200

    design basics

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,12 +8,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        primaryColor: Colors.cyan,
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Crafty Demo'),
     );
   }
 }
@@ -26,15 +33,60 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var textStyle = new TextStyle(
+    color: Colors.white,
+    fontSize: 20,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
+      body: Container(
+        padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+        height: 200,
+        decoration: BoxDecoration(
+          boxShadow: [
+            new BoxShadow(
+              color: Colors.black.withOpacity(0.8),
+              blurRadius: 10.0,
+            ),
+          ],
+          border: Border.all(color: true ? Colors.green : Colors.red, width: 8),
+          borderRadius: BorderRadius.all(Radius.circular(25)),
+          image: DecorationImage(
+              image: AssetImage("images/backgroundSample.png"),
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.4), BlendMode.darken)),
+        ),
+        margin: EdgeInsets.fromLTRB(15, 10, 15, 10),
         child: Column(
-          children: <Widget>[],
+          children: <Widget>[
+            Container(
+
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(25)),
+                  color: Colors.black54
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "My minecraft server",
+                    style: textStyle,
+                  ), Text(
+                    "10/20",
+                    style: textStyle,
+                  ),
+                ],
+              ),
+            ),
+            Container()
+          ],
         ),
       ),
     );
